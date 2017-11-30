e8ba8b0957d154e2154ba26dfa08a8e6e7ef584f
German Rojo Arce <garojar@hotmail.com>
2017-11-30 20:32:45 -0300

    * Se arregla clase Vehiculo

---
+++ app/src/main/java/cl/ucn/disc/dam/controlaucn/model/Vehiculo.java
@@ -0,0 +1,43 @@
+package cl.ucn.disc.dam.controlaucn.model;
+
+/**
+ * Clase Vehiculo
+ * @author Germán Rojo
+ *
+ */
+public final  class Vehiculo {
+
+    /**
+     * Persona que inscribio el vehiculo
+     */
+    private Persona dueño;
+    /**
+     * Placa patente
+     */
+    private String patente;
+
+    /**
+     * Marca productora
+     */
+    private String marca;
+
+    /**
+     * Color asociado
+     */
+    private String color;
+
+    /**
+     * Modelo = version
+     */
+    private String modelo;
+
+    /**
+     * año de fabricacion
+     */
+    private String year;
+
+    /**
+     * Descripcion opcional sobre el vehiculo
+     */
+    private String nota;
+}
